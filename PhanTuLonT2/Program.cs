f9d69d71af3851b3caaa2f5716656fe3a026e915
Tung <dtc2154802010680@ictu.edu.vn>
2023-09-14 10:39:29 +0700

    hitung101@users.noreply.github.com

---
+++ PhanTuLonT2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhanTuLonT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập dãy số, cách nhau bằng dấu cách: ");
+            string input = Console.ReadLine();
+
+            // Chuyển đổi chuỗi nhập vào thành mảng các số nguyên
+            string[] numbersStr = input.Split(' ');
+            int[] numbers = new int[numbersStr.Length];
+
+            for (int i = 0; i < numbersStr.Length; i++)
+            {
+                if (int.TryParse(numbersStr[i], out int num))
+                {
+                    numbers[i] = num;
+                }
+                else
+                {
+                    Console.WriteLine("Lỗi: Vui lòng nhập các số nguyên hợp lệ.");
+                    return;
+                }
+            }
+
+            if (numbers.Length < 2)
+            {
+                Console.WriteLine("Dãy số phải có ít nhất 2 phần tử.");
+                return;
+            }
+
+            int max1 = int.MinValue; // Giá trị lớn nhất
+            int max2 = int.MinValue; // Giá trị lớn thứ hai
+
+            foreach (int num in numbers)
+            {
+                if (num > max1)
+                {
+                    max2 = max1; // Cập nhật giá trị lớn thứ hai
+                    max1 = num; // Cập nhật giá trị lớn nhất
+                }
+                else if (num > max2 && num != max1)
+                {
+                    max2 = num; // Cập nhật giá trị lớn thứ hai
+                }
+            }
+
+            if (max2 == int.MinValue)
+            {
+                Console.WriteLine("Không có phần tử lớn thứ hai trong dãy số.");
+            }
+            else
+            {
+                Console.WriteLine("Phần tử lớn thứ hai trong dãy số là: " + max2);
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ TinhTOngNN/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhTOngNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập giá trị của n (n > 10): ");
+            int n;
+
+            if (int.TryParse(Console.ReadLine(), out n) && n > 10)
+            {
+                int sum = 0;
+
+                for (int i = 11; i <= n; i++)
+                {
+                    sum += i * 11; // Thêm i * 11 vào tổng
+                }
+
+                Console.WriteLine("Tổng của dãy S = 11 + 22 + 33 + ... + " + n + " là: " + sum);
+            }
+            else
+            {
+                Console.WriteLine("Vui lòng nhập một số nguyên lớn hơn 10.");
+            }
+            Console.ReadLine();
+        }
+       
+   
+        
+    }
+    
+}
