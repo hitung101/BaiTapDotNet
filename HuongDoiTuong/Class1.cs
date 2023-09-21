32a2a208124b59e1ce975afdce7154300a4a9e83
Tung <dtc2154802010680@ictu.edu.vn>
2023-09-21 23:56:33 +0700

    program.cs

---
+++ HuongDoiTuong/Class1.cs
@@ -0,0 +1,32 @@
+﻿using HuongDoiTuong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuongDoiTuong
+{
+   internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            //khoi tao doi tuong lop hoc sinh 
+            hocsinh Hocsinh = new hocsinh();
+         
+            //nhap thong tin sv
+            Console.WriteLine("Nhap thong tin hoc sinh ");
+            Hocsinh.NhapHS();
+            //
+            //hien thi doi tuong hoc sinh
+            Console.WriteLine("Hien thi thong tin hs theo contructor 1");
+            Hocsinh.XuatHS();
+            Console.WriteLine("Hien thi thong tin hs theo contructor 2, co doi tuong tryten vao");
+            Hocsinh.XuatHS();
+
+
+            Console.Read();
+        }
+
+    }
+}
---
+++ HuongDoiTuong/Hocsinh.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace HuongDoiTuong
+{
+    internal class hocsinh
+    {
+        // Khai báo thuộc tính
+        private int Mahs;
+        private string tenhs;
+        private int namsinh;
+        private string diachi;
+        private double diemToan;
+        private double diemVan;
+        private double diemAnh;
+
+        // Khai báo hàm khởi tạo đối tượng (Constructor)
+        public hocsinh()
+        {
+
+        }
+        
+        // Xây dựng hàm khởi tạo đối tượng
+        public hocsinh(int ma, string ten, int ns, string diaChi, double toan, double van, double anh)
+        {
+            Mahs = ma;
+            tenhs = ten;
+            namsinh = ns;
+            diachi = diaChi;
+            diemToan = toan;
+            diemVan = van;
+            diemAnh = anh;
+        }
+
+        public void NhapHS()
+        {
+            Console.Write("Nhap ma HS: ");
+            Mahs = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap ten sinh vien: ");
+            tenhs = Console.ReadLine();
+            Console.Write("Nhap nam sinh: ");
+            namsinh = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap dia chi: ");
+            diachi = Console.ReadLine();
+            Console.Write("Nhap diem toan: ");
+            diemToan = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nhap diem van: ");
+            diemVan = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Nhap diem anh: ");
+            diemAnh = Convert.ToDouble(Console.ReadLine());
+        }
+
+        // Viết hàm tính điểm trung bình
+        public double TinhDTB()
+        {
+            double DTB = Math.Round((diemAnh + diemToan + diemVan) / 3, 1);
+            return DTB;
+        }
+
+        public void XuatHS()
+        {
+            Console.WriteLine("Thong tin sinh vien la: ");
+            Console.WriteLine($"Ma HS: {Mahs}");
+            Console.WriteLine($"Ten HS: {tenhs}");
+            Console.WriteLine($"Nam Sinh: {namsinh}");
+            Console.WriteLine($"Dia chi: {diachi}");
+            Console.WriteLine($"Diem Trung Binh: {TinhDTB()}");
+        }
+    }
+}
